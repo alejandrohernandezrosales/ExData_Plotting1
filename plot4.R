2a1868ee7632712ed1bcabfac260b256b918fe94
alejandrohernandezrosales <ahernandezr@gmail.com>
2014-10-12 18:13:50 -0500

    changes to borders
    forgot to remove the legend borders on plot4.*

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-## plot4.R my code for the 4th plot of the assignment of week 1 
+## plot3.R my code for the 3rd plot of the assignment of week 1 
 ## of Exploratory Data Analysis
 
 #define the column classes
@@ -28,7 +28,7 @@ plot(x$datetime, x$Sub_metering_1, type="l",
 lines(x$datetime, x$Sub_metering_2, type="l",col="red")
 lines(x$datetime, x$Sub_metering_3, type="l",col="blue")
 #write the legend
-legend("topright", bty="n", lty= 1, col = c("black", "red", "blue"), 
+legend("topright", lty= 1, col = c("black", "red", "blue"), 
        legend = c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 #plot chart 1,2
