3baa3202f07dfc17189b994c47c4d2269722d44d
alejandrohernandezrosales <ahernandezr@gmail.com>
2014-10-12 18:15:42 -0500

    trying again to apply changes
    change to legend border

--- plot4.R
+++ plot4.R
@@ -28,7 +28,7 @@ plot(x$datetime, x$Sub_metering_1, type="l",
 lines(x$datetime, x$Sub_metering_2, type="l",col="red")
 lines(x$datetime, x$Sub_metering_3, type="l",col="blue")
 #write the legend
-legend("topright", lty= 1, col = c("black", "red", "blue"), 
+legend("topright", bty="n", lty= 1, col = c("black", "red", "blue"), 
        legend = c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 #plot chart 1,2
